335b2f62b17e0ffbe1dee4be5b31f020cdf80fa1
Ajay Ravi <ajayravi2208@gmail.com>
2023-02-02 20:19:39 +0530

    binary search sum 1

---
+++ src/com/ajay/BS_question_1.java
@@ -0,0 +1,33 @@
+package com.ajay;
+
+public class BS_question_1 {
+     //find target number or smallest greater number
+     public static void main(String[] args) {
+         int[] arr = {2,6,8,9,10,12,14,16,17,19};
+         int target = 15;
+         int res = BS(arr, target);
+         System.out.println(res + " " + arr[res]);
+     }
+
+     static int BS(int[] arr, int target){
+         int start = 0;
+         int end = arr.length - 1;
+
+         while (start <= end){
+             int mid = start + (end - start)/2;
+             int mid_ele = arr[mid];
+             if (mid_ele > target){
+                 end = mid -1;
+             }
+             else if(mid_ele < target){
+                 start = mid + 1;
+             }
+             else {
+                 return mid;
+             }
+         }
+         return start;
+     }
+
+
+}
