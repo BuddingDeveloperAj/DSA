90b2ab753c47b4d8dad0db33110ac0883771d0ca
Ajay Ravi <ajayravi2208@gmail.com>
2023-02-02 22:32:00 +0530

    floor of number in array

--- src/com/ajay/Floor_of_number.java
+++ src/com/ajay/Floor_of_number.java
@@ -3,15 +3,17 @@ package com.ajay;
 public class Floor_of_number {
     public static void main(String[] args) {
         int[] arr = {2,6,8,9,10,12,14,16,17,19};
-        int target = 15;
+        int target = 1;
         int res = BS(arr, target);
-        System.out.println(res + " " + arr[res]);
+        System.out.println(res);
     }
 
     static int BS(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
-
+        if (target < arr[0]){
+            return -1;
+        }
         while (start <= end){
             int mid = start + (end - start)/2;
             int mid_ele = arr[mid];
--- src/com/ajay/ceiling_of_number.java
+++ src/com/ajay/ceiling_of_number.java
@@ -4,14 +4,18 @@ public class ceiling_of_number {
      //find target number or smallest greater number
      public static void main(String[] args) {
          int[] arr = {2,6,8,9,10,12,14,16,17,19};
-         int target = 15;
+         int target = 20;
          int res = BS(arr, target);
-         System.out.println(res + " " + arr[res]);
+         System.out.println(res);
      }
 
      static int BS(int[] arr, int target){
          int start = 0;
          int end = arr.length - 1;
+
+         if (target > arr[arr.length - 1]){
+             return -1;
+         }
 
          while (start <= end){
              int mid = start + (end - start)/2;
