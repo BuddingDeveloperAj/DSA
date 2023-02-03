ddc84c583879d6c129e447daae94d4fb110f6e95
Ajay Ravi <ajayravi2208@gmail.com>
2023-02-03 23:10:11 +0530

    leetcode 744 python and js

--- src/com/bs_sums/leetcode744.java
+++ src/com/bs_sums/leetcode744.java
@@ -18,7 +18,41 @@ public class leetcode744 {
             }
         }
         System.out.println(letters[start % letters.length]);
+        System.out.println("hello");
     }
 }
 
+//python solution for the same
+//class Solution:
+//        def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+//        start = 0
+//        end = len(letters) - 1
+//
+//        while (start <= end):
+//        mid = (start + (end - start)//2)
+//        if (target < letters[mid]):
+//        end = mid - 1
+//        else:
+//        start = mid + 1
+//
+//        return letters[start % len(letters)]
 
+
+// javascirpt solution
+//    /**
+//     * @param {character[]} letters
+//     * @param {character} target
+//     * @return {character}
+//     */
+//    var nextGreatestLetter = function(letters, target) {
+//        let start = 0
+//        let end = letters.length - 1
+//
+//        while (start <= end){
+//        let mid = start + Math.floor((end - start)/2)
+//
+//        if (target < letters[mid]) end = mid -1
+//        else start = mid + 1
+//        }
+//        return letters[start % letters.length]
+//        };
