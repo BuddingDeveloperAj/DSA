a4dc9242751e555f997b0c0e47bc9eb516a66030
Ajay Ravi <ajayravi2208@gmail.com>
2023-06-17 23:08:08 +0530

    17/6 commits

---
+++ src/com/bitwise/Findnumwhichoccursonce.java
@@ -0,0 +1,4 @@
+package com.bitwise;
+
+public class Findnumwhichoccursonce {
+}
--- src/com/bitwise/operators.java
+++ src/com/bitwise/operators.java
@@ -2,6 +2,16 @@ package com.bitwise;
 
 public class operators {
     public static void main(String[] args) {
+        int a = 45;
+        int[] nums = {1,3,4};
+        System.out.println(findsingleElement(nums));
+    }
 
+    public static int findsingleElement(int[] arr){
+        int ans = 0;
+        for (int num:arr){
+            ans ^= num;
+        }
+        return ans;
     }
 }
