c3bdfe039e474518c10220860cc79481db2a46fc
Ajay Ravi <ajayravi2208@gmail.com>
2023-06-18 15:12:34 +0530

    17/6 commits

--- src/com/bitwise/Findnumwhichoccursonce.java
+++ src/com/bitwise/Findnumwhichoccursonce.java
@@ -1,4 +1,9 @@
 package com.bitwise;
 
 public class Findnumwhichoccursonce {
+    public static void main(String[] args) {
+//        int[] arr = {1,2,2,2,1,1,7,4,4,4};
+
+        System.out.println((1<<(4)));
+    }
 }
