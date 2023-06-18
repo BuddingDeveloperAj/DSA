1710cc50157e5909a88b67eae16bd5f973578996
Ajay Ravi <ajayravi2208@gmail.com>
2023-06-18 15:13:02 +0530

    17/6 commits

---
+++ src/com/bitwise/powerofnum.java
@@ -0,0 +1,18 @@
+package com.bitwise;
+
+public class powerofnum {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 6;
+        int ans = 1;
+        while (power>0){
+            if ((power&1) == 1){
+                ans *= base;
+            }
+            base *= base;
+            power = power>>1;
+        }
+
+        System.out.println(ans);
+    }
+}
